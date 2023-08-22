90ce5568e9692be0bd27e93f7cda45bd717c2718
ManupaDev <manupadevsuite@gmail.com>
2023-08-22 22:30:25 +0530

    feat: configs

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require("express");
+"use strict";
+
+import express from "express";
+
 const app = express();
-const port = 8000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.post("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
