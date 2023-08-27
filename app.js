31035b360c3b20c2763dc39e775c4fc4bb7659b1
ManupaDev <manupadevsuite@gmail.com>
2023-08-27 23:03:57 +0530

    feat: day 2 complete

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const devices = [];
+
+app.get("/devices", (req, res) => {
+  return res.status(200).json(devices);
 });
 
-app.post("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/devices/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(val);
+    const device = devices.find((el) => el.id === parseInt(id));
+    console.log(device);
+    if (!device) {
+      return res.status(404).send();
+    }
+    return res.status(200).send(device);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+app.post("/devices", (req, res) => {
+  const device = req.body;
+  devices.push(device);
+  return res.status(201).send();
 });
 
 const PORT = 8000;
